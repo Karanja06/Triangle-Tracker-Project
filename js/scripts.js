7cd5f534bc712dea25d2aec0387d3b9b739e55b1
karanja06 <kennkaranja85@gmail.com>
2019-06-02 16:20:22 +0300

    Change h1 back-ground color and font-style

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ var sideA=parseInt(prompt("Enter side A"));
 var sideB=parseInt(prompt("Enter side B"));
 var sideC=parseInt(prompt("Enter side C"));
 
+
+function myFunction()
+if (sideA===sideB===sideC) {
+    console.log("Equilateral Trianlge!!!");
+} else if(sideA===sideB
