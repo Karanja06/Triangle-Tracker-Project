ca87b03188d08c1f58518f235f846462b9906d5d
karanja06 <kennkaranja85@gmail.com>
2019-06-02 23:26:03 +0300

    Add the javascript logical operators as well as the console messages

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ var sideB=parseInt(prompt("Input Value: sideB"));
 var sideC=parseInt(prompt("Input Value: sideC"));
 
 
-if (sideA==sideB==sideC) { 
+if (sideA==sideB & sideA==sideC) { 
     console.log("Equilateral Triangle!!!");
-} else {
-    console.log("Ooops! Not an equilateral triangle.")
+} else if (sideA==sideB | sideA==sideC |sideB==sideC) {
+    console.log("This is an Isosceles triangle!!!")
+} else if ((sideA!=sideB & sideA!=sideC & sideB!=sideC) && (sideA+sideB>sideC | sideB+sideC>sideA | sideA+sideC>sideB)) {
+    console.log("Voila! Its a Scalene triangle!!")
+} else if (sideA+sideB<=sideC | sideA+sideC<=sideB | sideB+sideC<=sideA)
+console.log("Ooops! This is not a triangle!!")
+else {
+    console.log("Not an equilateral triangle.")
 }
