6808da4ac4d0dd76c748660b520b6242a0004c29
karanja06 <kennkaranja85@gmail.com>
2019-06-02 20:10:28 +0300

    Add form input elements including input type and input names

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-var sideA=parseInt(prompt("Enter side A"));
-var sideB=parseInt(prompt("Enter side B"));
-var sideC=parseInt(prompt("Enter side C"));
+var sideA=parseInt(prompt("Input Value: sideA"));
+var sideB=parseInt(prompt("Input Value: sideB"));
+var sideC=parseInt(prompt("Input Value: sideC"));
 
 
-function myFunction()
-if (sideA===sideB===sideC) {
-    console.log("Equilateral Trianlge!!!");
-} else if(sideA===sideB
+if (sideA==sideB==sideC) { 
+    console.log("Equilateral Triangle!!!");
+} else {
+    console.log("Ooops! Not an equilateral triangle.")
+}
