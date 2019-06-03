969e261b42e8ff62de3861d74fab1478fc42d6e6
karanja06 <kennkaranja85@gmail.com>
2019-06-03 10:59:39 +0300

    Add input label styles in css

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,18 @@ var sideC=parseInt(prompt("Input Value: sideC"));
 
 
 if (sideA==sideB & sideA==sideC) { 
-    console.log("Equilateral Triangle!!!");
-} else if (sideA==sideB | sideA==sideC |sideB==sideC) {
-    console.log("This is an Isosceles triangle!!!")
-} else if (sideA!=sideB & sideA!=sideC & sideB!=sideC) {
-    console.log("Voila! Its a Scalene triangle!!")
-} else if (sideA+sideB>sideC | sideB+sideC>sideA | sideA+sideC>sideB) {
-console.log("Voila! Its a Scalene triangle!!")
-} else {
-    console.log("Ooops! This is not a triangle!!")
+    alert("Equilateral Triangle!!!");
+} 
+else if (sideA==sideB | sideA==sideC |sideB==sideC); {
+    alert("This is an Isosceles triangle!!!")
+} 
+else if (sideA!=sideB & sideA!=sideC & sideB!=sideC) && ((sideA+sideB)>sideC | (sideB+sideC)>sideA | (sideA+sideC)>sideB); {
+    alert("Voila! Its a Scalene triangle!!")
+} 
+else if ((sideA+sideB)<=sideC || (sideB+sideC)<=sideA || (sideC+sideA)<=sideB); {
+    alert("Ooops! This is not a triangle!!")
 }
+else (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)); {
+    alert("No values entred!!!")
+}
+    
