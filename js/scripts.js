9f5d8cf80234497e06995d965e193b5ddba7c72d
karanja06 <kennkaranja85@gmail.com>
2019-06-03 11:26:49 +0300

    Modify the equal to boolean operators

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,15 @@ var sideB=parseInt(prompt("Input Value: sideB"));
 var sideC=parseInt(prompt("Input Value: sideC"));
 
 
-if (sideA==sideB & sideA==sideC) { 
+if (sideA===sideB && sideA===sideC) { 
     alert("Equilateral Triangle!!!");
-} 
-else if (sideA==sideB | sideA==sideC |sideB==sideC); {
+} else if (sideA===sideB || sideA===sideC || sideB===sideC) {
     alert("This is an Isosceles triangle!!!")
-} 
-else if (sideA!=sideB & sideA!=sideC & sideB!=sideC) && ((sideA+sideB)>sideC | (sideB+sideC)>sideA | (sideA+sideC)>sideB); {
+} else if (sideA!=sideB && sideA!=sideC && sideB!=sideC) {
+    alert("Voila! Its a Scalene triangle!!")
+} else if (sideA+sideB>sideC || sideB+sideC>sideA || sideA+sideC>sideB) {
     alert("Voila! Its a Scalene triangle!!")
 } 
-else if ((sideA+sideB)<=sideC || (sideB+sideC)<=sideA || (sideC+sideA)<=sideB); {
-    alert("Ooops! This is not a triangle!!")
-}
-else (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)); {
-    alert("No values entred!!!")
-}
-    
+    else {
+        alert("Ooops! This is not a triangle!!")
+    }
